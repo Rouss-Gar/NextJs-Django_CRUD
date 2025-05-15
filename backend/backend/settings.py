c8ea449043671e551080a95710317bfcd2d1d687
Rouss-Gar <rosmerygar2003@gmail.com>
2025-05-15 06:44:24 -0600

    init: configuración inicial de proyecto Django y Next.js

---
+++ backend/backend/settings.py
@@ -0,0 +1,18 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'api',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    ...
+]
+
+CORS_ORIGIN_ALLOW_ALL = True  # o configura un dominio específico
